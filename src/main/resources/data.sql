591328490f638d0edb70a904e2a6bcb9697494fc
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2024-05-08 14:24:38 -0300

    colocado properties e data.sql para geração de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO party (morale) VALUES (80);
+INSERT INTO hero (name, health, mana, party_id)
+VALUES ('Hero1', 100, 100, 1);
+INSERT INTO hero (name, health, mana, party_id)
+VALUES ('Hero2', 100, 100, 1);
+
+INSERT INTO hero (name, health, mana, party_id)
+VALUES ('Hero3', 100, 100, 1);
+INSERT INTO hero (name, health, mana, party_id)
+VALUES ('Hero4', 100, 100, 1);
+INSERT INTO hero (name, health, mana, party_id)
+VALUES ('Hero5', 100, 100, 1);
